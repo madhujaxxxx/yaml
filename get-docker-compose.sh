d27d770936e21a182837a1417dc83c0c3278bac4
KEINOS <github@keinos.com>
2019-04-16 01:27:40 +0900

    chore: Readable indentation

--- get-docker-compose.sh
+++ get-docker-compose.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-
-echo '====> Installing latest docker compose'
+#  Install latest docker compose
+# ===============================
 
 which docker-compose > /dev/null 2>&1
 if [ $? -eq 0 ]; then
@@ -16,4 +16,4 @@ path_dir_bin='/usr/local/bin'
 path_file_docker_compose="${path_dir_bin}/docker-compose"
 
 curl -sL "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o $path_file_docker_compose
-sudo chmod +x $path_file_docker_compose
+chmod +x $path_file_docker_compose
